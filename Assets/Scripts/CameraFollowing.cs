23bc7893e91d4032d7afcc4f2f0861f9822c70e4
Ghostmoonk <jeremy.tapia30@hotmail.com>
2019-12-02 12:59:15 +0100

    Camera following player

---
+++ Assets/Scripts/CameraFollowing.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowing : MonoBehaviour
+{
+    #region Components
+    [SerializeField] Transform targetToFollow;
+    #endregion
+
+    #region Variables
+    [SerializeField] float cameraSlideSpeed;
+    Quaternion initialRotation;
+    float xOffset;
+    #endregion
+
+    private void Start()
+    {
+        initialRotation = transform.rotation;
+
+        xOffset = targetToFollow.position.x - transform.position.x;
+    }
+    void Update()
+    {
+        if (targetToFollow != null)
+        {
+            FollowTarget();
+        }
+    }
+
+    private void FollowTarget()
+    {
+        Vector3 finalPosition = new Vector3(targetToFollow.position.x - xOffset, transform.position.y, transform.position.z);
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, finalPosition, cameraSlideSpeed);
+        transform.position = smoothPosition;
+    }
+}
