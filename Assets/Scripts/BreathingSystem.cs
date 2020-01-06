b4c03f01f69d07bc555aa0d0d66f569324ef865b
Sylve <kikar4u@gmail.com>
2020-01-06 22:01:30 +0100

    Ajout triggerbox, test shader neige, cap cercle de respiration bugger, et moult autres joies

--- Assets/Scripts/BreathingSystem.cs
+++ Assets/Scripts/BreathingSystem.cs
@@ -8,21 +8,23 @@ public class BreathingSystem : MonoBehaviour
     Player player;
     #region Circles
     // cercle player
-    [SerializeField]Image innerCircle;
+    [SerializeField] Image innerCircle;
     RectTransform innerCircleTransform;
     // cercle avatar
-    [SerializeField]Image outerCircle;
+    [SerializeField] Image outerCircle;
     RectTransform outerCircleTransform;
     [Range(0.01f,0.1f)]
-    [SerializeField] float breathSpeed;
-    [SerializeField] float scaleTimeStep;
+    public float breathSpeed;
+    public float scaleTimeStep;
+    public float capPlayerInnerCircleMax;
+    public float capPlayerInnerCircleMin;
     Vector3 originalScale;
     #region Input
     float LeftTrigger;
     float RightTrigger;
     #endregion
-    [SerializeField] AnimationCurve breathCurve;
-    [SerializeField] float speedCirclePlayer;
+    public AnimationCurve breathCurve;
+    public float speedCirclePlayer;
     #endregion
 
     #region Collider
@@ -38,58 +40,91 @@ public class BreathingSystem : MonoBehaviour
         outerCircleTransform = outerCircle.GetComponent<RectTransform>();
 
         originalScale = outerCircleTransform.localScale;
-
+        StartCoroutine(BreathScaling(breathSpeed));
         //Debug.Log(" Breath courb : " + breathCurve);
 
-        StartCoroutine(BreathScaling(breathSpeed));
+
     }
 
     private void Update()
-    {   // on récupère les valeurs des deux gachettes
+    {
         LeftTrigger = Input.GetAxis("LeftTrigger");
         RightTrigger = Input.GetAxis("RightTrigger");
         //Debug.Log("right trigger : " + RightTrigger);
         //Debug.Log("Left trigger" + LeftTrigger);
         // si leurs valeurs sont plus grand (donc appui de la part du joueur) on grandit le cercle
-        if(LeftTrigger >= 0.1f || RightTrigger >= 0.1f)
+        if (LeftTrigger >= 0.1f || RightTrigger >= 0.1f)
         {
+
             // NOTE : J'ai mis un 2 pour augmenter la vitesse, mais il doit y avoir moyen de faire autrement
-            innerCircleTransform.localScale += new Vector3((LeftTrigger + RightTrigger) * Time.deltaTime * 2, (LeftTrigger + RightTrigger) * Time.deltaTime * 2, 0.0f);
+            // BUG : les conditions font buguer le système, avec les conditions le cercle n'est plus contrôlable, du moins, il bloque à une certaines distance
+            //if (innerCircle.rectTransform.localScale.x >= capPlayerInnerCircleMin)
+            //{
+            innerCircleTransform.localScale -= new Vector3((LeftTrigger + RightTrigger) * Time.deltaTime * 2, (LeftTrigger + RightTrigger) * Time.deltaTime * 2, 0.0f);
+            //}
+
+
         }
         // sinon on fait rétrécir le cercle
         else
         {
-            innerCircleTransform.localScale -= new Vector3((speedCirclePlayer) * Time.deltaTime, (speedCirclePlayer) * Time.deltaTime, 0.0f);
+            // cap pour par que le cercle ne dépasse de l'écran si le joueur ne fait rien
+            // BUG : les conditions font buguer le système, avec les conditions le cercle n'est plus contrôlable, du moins, il bloque à une certaines distance
+            //if (innerCircle.rectTransform.localScale.x <= capPlayerInnerCircleMax)
+            //{
+            //Debug.Log("fjqdsi" + innerCircleTransform.localScale.x);
+            innerCircleTransform.localScale += new Vector3((speedCirclePlayer) * Time.deltaTime, (speedCirclePlayer) * Time.deltaTime, 0.0f);
+            //}
+
         }
-        
+
         //Si le cercle du player est dans le cercle de l'outer
-        if (outerMarginCollider.bounds.Contains(new Vector3(playerBreathCollider.bounds.max.x,playerBreathCollider.bounds.center.y, 0f))
-         && !innerMarginCollider.bounds.Contains(new Vector3(playerBreathCollider.bounds.max.x,playerBreathCollider.bounds.center.y, 0f))){
+        if (outerMarginCollider.bounds.Contains(new Vector3(playerBreathCollider.bounds.max.x, playerBreathCollider.bounds.center.y, 0f))
+         && !innerMarginCollider.bounds.Contains(new Vector3(playerBreathCollider.bounds.max.x, playerBreathCollider.bounds.center.y, 0f)))
+        {
             //Le joueur respire bien
             Debug.Log("Tu respires bien, gg !");
-        }else{
+        }
+        else
+        {
             //Le joueur respire mal
             Debug.Log("Tu respires mal connard !");
         }
+        Debug.Log("shit here we go again");
 
     }
 
-    IEnumerator BreathScaling(float Speed){
-        while(true){
-            
-            outerCircleTransform.localScale += new Vector3(Speed * breathCurve.Evaluate(Time.time), Speed * breathCurve.Evaluate(Time.time), 0.0f);
-            
-            yield return new WaitForSeconds(Time.deltaTime);
+    public void CallBreathingSystem(float breathSpeed2, float scaleTimeStep2, float capPlayerInnerCircleMax2, float capPlayerInnerMin2, AnimationCurve breathCurve2, float speedCirclePlayer2)
+    {
 
-            if(outerCircleTransform.localScale.x >= 3.0f)
+        breathSpeed = breathSpeed2;
+        capPlayerInnerCircleMax = capPlayerInnerCircleMax2;
+        capPlayerInnerCircleMin = capPlayerInnerMin2;
+        breathCurve = breathCurve2;
+        speedCirclePlayer = speedCirclePlayer2;
+
+    }
+    public IEnumerator BreathScaling(float Speed){
+        Debug.Log("breathSpeed" + breathSpeed);
+        Debug.Log("BreathCurve" + breathCurve);
+        Debug.Log("Speed circle player" + speedCirclePlayer);
+        while (true){
+            Debug.Log("outerscaleBefore" + outerCircleTransform.localScale);
+            outerCircleTransform.localScale += new Vector3(Speed * breathCurve.Evaluate(Time.time), Speed * breathCurve.Evaluate(Time.time), 0.0f);
+            Debug.Log("outerscale" + outerCircleTransform.localScale);
+            yield return new WaitForSeconds(0.01f);
+            if (outerCircleTransform.localScale.x >= 1.5f)
             {
                 Speed = -Speed;
+                Debug.Log("shit here=");
             }
 
             if(outerCircleTransform.localScale.x <= originalScale.x)
             {
                 Speed = -Speed;
             }
+
+
         }
     }
 }
---
+++ Assets/Scripts/TriggerBreathing.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerBreathing : MonoBehaviour
+{
+    Player player;
+
+    [SerializeField] GameObject breathingHUD;
+    [SerializeField] GameObject HUD;
+    // NOTE : On a plus besoin d'avoir ces paramètres en public , car on les donne dans TriggerBreathing à la place, ici, on s'occupe juste des sprites 
+    // et des animations, de la vérification des collider et que le joueur joue bien
+    [Range(0.01f, 0.1f)]
+    [SerializeField] float breathSpeed;
+    [SerializeField] float scaleTimeStep;
+    [SerializeField] float capPlayerInnerCircleMax;
+    [SerializeField] float capPlayerInnerCircleMin;
+    Vector3 originalScale;
+    #region Input
+    float LeftTrigger;
+    float RightTrigger;
+    #endregion
+    [SerializeField] AnimationCurve breathCurve;
+    [SerializeField] float speedCirclePlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        BreathingSystem breathingVar = breathingHUD.GetComponent<BreathingSystem>();
+        if (other.tag == "Player")
+        {
+            // on set toutes les variables données en paramètres par la triggerbox au script de respiration
+            breathingVar.breathSpeed = breathSpeed;
+            breathingVar.scaleTimeStep = scaleTimeStep;
+            breathingVar.capPlayerInnerCircleMax = capPlayerInnerCircleMax;
+            breathingVar.capPlayerInnerCircleMin = capPlayerInnerCircleMin;
+            breathingVar.breathCurve = breathCurve;
+            breathingVar.speedCirclePlayer = speedCirclePlayer;
+            // on appelle la fonction avec les paramètres mis dans le trigger
+            // breathingHUD.GetComponent<BreathingSystem>().CallBreathingSystem(breathSpeed, scaleTimeStep, capPlayerInnerCircleMax, capPlayerInnerCircleMin, breathCurve, speedCirclePlayer);
+            Instantiate(breathingHUD, HUD.transform);
+            // on appelle la fonction, avec les paramètres pour ce trigger
+
+
+            Debug.Log("C'est le joueur qui passe par là");
+            //Instantiate(BreathingHUD, HUD.transform);
+        }
+
+    }
+}
