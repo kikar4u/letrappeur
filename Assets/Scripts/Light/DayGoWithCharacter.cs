b738db008d856875564c7ffade4ce27e917ee84c
Martin <valleixmartin89@gmail.com>
2020-02-14 10:53:39 +0100

    Soleil

--- Assets/Scripts/Light/DayGoWithCharacter.cs
+++ Assets/Scripts/Light/DayGoWithCharacter.cs
@@ -10,10 +10,11 @@ public class DayGoWithCharacter : MonoBehaviour
     [SerializeField] private float ratioLight =1;
     private Vector3 savedPosition;
     private float H, S, V;
+    [SerializeField] private float dayAtTheBeginning =0 , dayAtTheEnd=90;
+    [SerializeField] private float luminosityAtTheBeginning=5, luminosityAtTheEnd=25;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -23,9 +24,9 @@ public class DayGoWithCharacter : MonoBehaviour
         {
             if (player.GetDirection() >= 0 && transform.position.x >= savedPosition.x)
             {
-                DirectionalLight.transform.eulerAngles = new Vector3(Mathf.Clamp(DirectionalLight.transform.eulerAngles.x+player.movementOffset * ratioLight, 0, 90), DirectionalLight.transform.eulerAngles.y, DirectionalLight.transform.eulerAngles.z);
+                DirectionalLight.transform.eulerAngles = new Vector3(Mathf.Clamp(DirectionalLight.transform.eulerAngles.x+player.movementOffset * ratioLight, dayAtTheBeginning, dayAtTheEnd), DirectionalLight.transform.eulerAngles.y, DirectionalLight.transform.eulerAngles.z);
                 Color.RGBToHSV((DirectionalLight.GetComponent<Light>().color),out H, out S, out V);
-                S=Mathf.Lerp(0.05f, 0.25f, Mathf.InverseLerp(0,90, DirectionalLight.transform.eulerAngles.x));
+                S=Mathf.Lerp(luminosityAtTheBeginning/100, luminosityAtTheEnd/100, Mathf.InverseLerp(dayAtTheBeginning,dayAtTheEnd, DirectionalLight.transform.eulerAngles.x));
                 Debug.Log(S);
                 Debug.Log("H" +H+"S"+ S+"V"+ V);
                 DirectionalLight.GetComponent<Light>().color = Color.HSVToRGB(H, S, V);
