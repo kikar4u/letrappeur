d3c628ce4f8fbc34cc24da8063b1093859a4a3e0
Martin <valleixmartin89@gmail.com>
2020-03-08 00:47:42 +0100

    DestroyableTreeAndMoreThings

---
+++ Assets/Scripts/Menu/Menu_Browser.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu_Browser : MonoBehaviour
+{
+    [SerializeField] Button[] menuButtons;
+
+    void Start()
+    {
+        menuButtons[0].Select();
+    }
+}
