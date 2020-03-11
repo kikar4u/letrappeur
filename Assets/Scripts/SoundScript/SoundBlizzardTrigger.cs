7bc8be3794071669679aab5616faf8e417d784b7
kikar4u <kikar4u@gmail.com>
2020-03-11 16:08:17 +0100

    Ajout music track

---
+++ Assets/Scripts/SoundScript/SoundBlizzardTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundBlizzardTrigger : MonoBehaviour
+{
+    [SerializeField] AudioSource[] Audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Audio[0].isPlaying)
+        {
+            GameObject.FindGameObjectWithTag("SoundManager").GetComponent<_MGR_SoundDesign>().PlaySound("Ambiance", Audio[0]);
+        }
+    }
+}
