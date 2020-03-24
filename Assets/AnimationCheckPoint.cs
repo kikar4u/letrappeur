2c7414faf9832817ae7788345725d225a4ea1bf3
kikar4u <kikar4u@gmail.com>
2020-03-24 22:25:59 +0100

    Ajout son placeholder checkpoint
    A l'aide j'aime bien l'idée mais ça colle pas à la DA

--- Assets/AnimationCheckPoint.cs
+++ Assets/AnimationCheckPoint.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class AnimationCheckPoint : MonoBehaviour
 {
-    [SerializeField] Material emissive;
+    [SerializeField] AudioSource checkpointSound;
     // Start is called before the first frame update
     void Start()
     {
-       // DynamicGI.SetEmissive(GetComponent<Renderer>(), Color.red);
+       
     }
-
+    public void PlayCheckPointSound()
+    {
+        GameObject.FindGameObjectWithTag("Managers").GetComponent<_MGR_SoundDesign>().PlaySound("CheckpointSound", checkpointSound);
+    }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
