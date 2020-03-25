35c4578e5bc514fcac0d2e70aedba62f3b45437a
yann.kukolewsky@hotmail.fr <Galgen1999>
2020-03-25 20:30:35 +0100

    Application musiques
    =ajustement des ambiances

---
+++ Assets/MusicTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTrigger : MonoBehaviour
+{
+    public AudioClip clip;
+    public AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider objet)
+    {
+        if(objet.gameObject.tag == "Player")
+            GameObject.FindGameObjectWithTag("Managers").GetComponent<_MGR_SoundDesign>().PlaySpecificSound(clip, source);
+    }
+}
