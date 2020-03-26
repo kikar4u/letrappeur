f594fd739d5fd17eec1194488a4dc50dcf9843e9
kikar4u <kikar4u@gmail.com>
2020-03-26 17:56:08 +0100

    Ajout menu options, graphisme et sons

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,23 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using TMPro;
+using UnityEngine.Audio;
 
 public class MenuManager : MonoBehaviour
 {
+    Dictionary<int, string> leDicoDesSettings = new Dictionary<int, string>();
+    List<string> settings = new List<string>();
+    [SerializeField] TMP_Dropdown graphics;
+    [SerializeField] TMP_Text label;
+    int currentSettings;
+
+    [SerializeField] Slider AmbientVolume;
+    [SerializeField] Slider Music;
+    [SerializeField] Slider SFX;
+    [SerializeField] Slider masterMix;
+    [SerializeField] AudioMixer mixer;
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+
+        currentSettings = QualitySettings.GetQualityLevel();
+
+        for (int i = 0; i < QualitySettings.names.Length; i++)
+        {
+            
+            Debug.Log(QualitySettings.names.Length);
+            settings.Add(QualitySettings.names[i]);
+            leDicoDesSettings.Add(i, QualitySettings.names[i]);
+        }
+        graphics.AddOptions(settings);
+    }
+    private void Start()
+    {
+        float masterValue;
+        if (mixer.GetFloat("Master", out masterValue))
+        {
+            masterMix.value = masterValue;
+            Debug.Log("caca prout" + masterValue);
+        }
+        float AmbianceValue;
+        if (mixer.GetFloat("Ambiance", out AmbianceValue))
+        {
+            AmbientVolume.value = AmbianceValue;
+            Debug.Log("caca prout" + AmbianceValue);
+        }
+        float SFXValue;
+        if (mixer.GetFloat("SFX", out SFXValue))
+        {
+            SFX.value = SFXValue;
+            Debug.Log("caca prout" + SFXValue);
+        }
+        float musicValue;
+        if (mixer.GetFloat("Musique", out musicValue))
+        {
+            Music.value = musicValue;
+            Debug.Log("caca prout" + musicValue);
+        }
+    }
+    public void changeSlider(Slider slider)
+    {
+        //Debug.Log(mixer);
+        string name = slider.name;
+        switch (name)
+        {
+            case "Master":
+                mixer.SetFloat("Master", slider.value);//
+                break;
+            case "AmbientVolume":
+                mixer.SetFloat("Ambiance", slider.value);
+                break;
+            case "SFX":
+                mixer.SetFloat("SFX", slider.value);//
+                break;
+            case "AmbianceMusique":
+                mixer.SetFloat("Musique", slider.value);
+                break;
+            default:
+                break;
+        }
+    }
+    public void ChangeGraphicSettings()
+    {
+        QualitySettings.SetQualityLevel(graphics.value, true);
+        Debug.Log("Current : " + QualitySettings.GetQualityLevel());
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-    public void Options()
-    {
-
     }
     public void ExitGame()
     {
