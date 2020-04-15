61318f0c187c972d388fae13a9cac70adfe824f9
Abhorash <valleixmartin89@gmail.com>
2020-04-15 19:04:28 +0200

    FogInProgress

---
+++ Assets/Scripts/Camera/Test_Fog.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Test_Fog : MonoBehaviour
+{
+    public Material material;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, material);
+    }
+
+    void Start()
+    {
+        Camera.main.depthTextureMode = DepthTextureMode.Depth;
+    }
+}
