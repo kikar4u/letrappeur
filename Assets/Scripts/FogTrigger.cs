db458564e1ae102e75816387dc70b4685456e2a4
Jeremy <jeremy.tapia30@hotmail.com>
2020-05-08 16:19:28 +0200

    Fog trigger

---
+++ Assets/Scripts/FogTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Ce script achève totalement le Single responsabilities du S des principes SOLID
+public class FogTrigger : MonoBehaviour
+{
+    [Range(0, 0.1f)]
+    [SerializeField] float finalIntensity;
+    [SerializeField] float transitionTime;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            CallFogUpdate();
+            Destroy(gameObject);
+        }
+    }
+    public void CallFogUpdate()
+    {
+        PostProcessManager.Instance.UpdateFogIntensity(finalIntensity, transitionTime);
+    }
+}
