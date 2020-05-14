92cf8c7ffc692c43174544a8d65f7bbbc9ebd6cb
kikar4u <sylve.renault@yahoo.fr>
2020-05-14 16:24:40 +0200

    Ajout sons effort

---
+++ Assets/EffortPlay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffortPlay : MonoBehaviour
+{
+    [SerializeField] AudioSource audiosource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void playEffort()
+    {
+        _MGR_SoundDesign.Instance.PlaySound("Effort", audiosource);
+    }
+}
